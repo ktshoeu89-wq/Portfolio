c6a7334ef66593fd66f09daad5596ddd54020bb9
ktshoeu89-wq <ktshoeu89@gmail.com>
2026-02-02 19:45:11 +0200

    first commit

---
+++ projects-data.js
@@ -0,0 +1,83 @@
+const projectsData = {
+    "kings-school": {
+        title: "High School website",
+        subtitle: "Developed for Kings Christian School",
+        role: "Frontend Developer & Designer",
+        tech: [
+        "HTML5 – semantic structure",
+        "CSS3 – layout, responsiveness, animations",
+        "JavaScript – interactivity and UI behavior"
+
+        ],
+        overview: "Project overview text here...",
+        problem: "Problem statement text here...",
+        solution: "Solution explanation here...",
+        features: [
+        "Responsive layout",
+        "Clear navigation",
+        "Dedicated academic sections"
+        ],
+        video: "images/school-video.webm",
+        screenshots: [
+        "images/single-room.jpg",
+        "images/single-room.jpg",
+        "images/single-room.jpg"
+        ],
+        live: "#",
+        github: "#",
+        result: "The site is now live and receiving bookings."
+    },
+
+    "molimo-lodge": {
+        title: "Guest Lodge website",
+        subtitle: "Developed for Molimo Nthuse Lodge",
+        role: "Frontend Developer",
+        tech: [
+        "HTML5",
+        "CSS3",
+        "JavaScript"
+        ],
+        overview: "Overview for lodge project...",
+        problem: "Problem for lodge...",
+        solution: "Solution for lodge...",
+        features: [
+        "Online booking layout",
+        "Gallery showcase",
+        "Mobile-first design"
+        ],
+        video: "images/lodge-video.webm",
+        screenshots: [
+        "images/single-room.jpg"
+        ],
+        live: "#",
+        github: "#",
+        result: "The site is now live and receiving bookings."
+    },
+
+        "bella-rosa": {
+        title: "Guest house website",
+        subtitle: "Developed for Bella Rosa Guest house",
+        role: "Frontend Developer & Designer",
+        tech: [
+        "HTML5",
+        "CSS3",
+        "JavaScript"
+        ],
+        overview: "Overview for guest house project...",
+        problem: "Problem for guest house...",
+        solution: "Solution for guest house...",
+        features: [
+        "Online booking layout",
+        "Gallery showcase",
+        "Mobile-first design"
+        ],
+        video: "images/lodge-video.webm",
+        screenshots: [
+        "images/single-room.jpg",
+        "images/single-room.jpg"
+        ],
+        live: "#",
+        github: "#",
+        result: "The site is now live and receiving bookings."
+    }
+};
